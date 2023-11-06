7c4e36f57cab484e12eafc74c0dd15009f97528a
BetoIglesias <betoiglesias010@gmail.com>
2023-11-06 12:05:29 -0300

    SellerFormViewModel e componente select para Departamento

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,31 @@
+﻿using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+   }
+
+ }
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 
 namespace SalesWebMvc
@@ -42,6 +43,9 @@ namespace SalesWebMvc
                     options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
