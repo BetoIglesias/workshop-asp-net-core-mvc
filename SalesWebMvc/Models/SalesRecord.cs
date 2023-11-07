ef5d43d63bdbf3ba3229d5a702f0bcd0c7ec19c8
BetoIglesias <betoiglesias010@gmail.com>
2023-11-07 15:17:34 -0300

    Implementando Busca agrupada

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id  { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double Valor { get; set; }
         public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -33,5 +33,24 @@ namespace SalesWebMvc.Services.SalesRecordService
                 .OrderByDescending(x => x.Data)
                 .ToListAsync();
         }
+        public async Task<List< IGrouping<Department, SalesRecord> >> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Data)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+
     }
 }
